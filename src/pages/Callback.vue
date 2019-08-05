b617f2d3056ffa237472fb9582981b6b4b2a0733
Unknown <fotterly@gmail.com>
2019-08-05 14:51:44 +1200

    Routes + Callback

---
+++ src/pages/Callback.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+	'This is a callback page for twitter stuff'
+  </div>
+</template>
+<script>
+export default {
+	name: 'callback',
+	data () {
+		return {
+			oauth_token: '',
+			oauth_verifier: ''
+		}  
+	},
+	methods: {
+		goBack(event){
+			event.preventDefault();
+			this.$router.go(-1);
+		},	
+	},
+	mounted () {
+		this.oauth_token = this.$route.query.oauth_token;
+		this.oauth_verifier = this.$route.query.oauth_verifier;
+		//some backend call goes here
+	}
+
+}
+
+</script>
--- src/routes.js
+++ src/routes.js
@@ -3,29 +3,35 @@ import Sheet from './pages/Sheet.vue'
 import Lobby from './pages/Lobby.vue'
 import Cluster from './pages/Cluster.vue'
 import Fail from './pages/404.vue'
+import Callback from './pages/Callback.vue'
 
 export default [
-{ 
-	path: '/',
-	component: Home
-},
-{
-	path: '/lobby',
-	component: Lobby
-},
-{
-	path: '/lobby/:cluster',
-	component: Cluster,
-	props: true
-	
-},
-{
-	path: '/lobby/:cluster/:sheet',
-	component: Sheet,
-	props: true
-},
-{
-	path: '*',
-	component: Fail
-}
+	{ 
+		path: '/',
+		component: Home
+	},
+	{
+		path: '/lobby',
+		component: Lobby
+	},
+	{
+		path: '/lobby/:cluster',
+		component: Cluster,
+		props: true
+		
+	},
+	{
+		path: '/lobby/:cluster/:sheet',
+		component: Sheet,
+		props: true
+	},
+	{
+		path: '/twitter/callback',
+		component: Callback
+		
+	},
+	{
+		path: '*',
+		component: Fail
+	}
 ]
