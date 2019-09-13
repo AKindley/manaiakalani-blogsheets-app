82214723682f0fa3b99675b0fa108b7538ed70d4
Adam M <fotterly@gmail.com>
2019-09-13 17:28:35 +1200

    Google login cleaned up and fixed
    Google button added, login fixed. Need to implement the whitelisting and login watching.

--- src/expressRoutes/authRoutes.js
+++ src/expressRoutes/authRoutes.js
@@ -5,9 +5,25 @@ var secret = require('../secret.json');
 var server = config.serverAddress;
 var client = config.clientAddress;
 var TWITTER_CONSUMER_KEY = secret.TWITTER_API_KEY;
-var TWITTER_CONSUMER_SECRET = secret.TWITTER_API_KEY_SECRET
-var passport = require('passport'), TwitterStrategy = require('passport-twitter').Strategy;
+var TWITTER_CONSUMER_SECRET = secret.TWITTER_API_KEY_SECRET;
+var googleClient = secret.GOOGLE_CLIENT_ID;
+var googleSecret = secret.GOOGLE_CLIENT_SECRET;
+var passport = require('passport'), TwitterStrategy = require('passport-twitter').Strategy, GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var Cluster = require('../models/clusterStructure');
+
+passport.use(new GoogleStrategy({
+	clientID: googleClient,
+	clientSecret: googleSecret,
+	callbackURL: server + "/auth/google/callback"
+	},
+	function(accessToken, refreshToken, user, done){
+		passport.serializeUser((user, done) => done(null, user)); //serializeUser and deserializeUser are important for avoiding passport errors. 
+		passport.deserializeUser((user, done) => done(null, user));
+		console.log(user);
+		return done(null, user);
+	}
+));
+
 passport.use(new TwitterStrategy({ //passport strategy for twitter auth
 		consumerKey: TWITTER_CONSUMER_KEY, //app key from the twitter dev dashboard
 		consumerSecret: TWITTER_CONSUMER_SECRET,// app key secret from the twitter dev dashboard
@@ -32,6 +48,7 @@ passport.use(new TwitterStrategy({ //passport strategy for twitter auth
 		return done(null, user); //fire redirect event for client
 	}
 ));
+
 	app.get('/auth/twitter/test/:cluster', function(req, res, next){ //initial auth call, where passport begins the auth 1.0a process
 		req.session.cluster = req.params.cluster; //saves the cluster id to the session
 		passport.authenticate('twitter')(req,res,next); //begins the auth chain
@@ -41,5 +58,15 @@ passport.use(new TwitterStrategy({ //passport strategy for twitter auth
 		{successRedirect: client + '/auth/twitter/callback',  //redirect for a successful auth chain
 		failureRedirect: client + '/404'} //redirect for a failed auth chain
 	));
+	
+	app.get('/auth/google', passport.authenticate('google', //TODO: monitoring whether they're signed in with the correct domain.
+		{ scope: ['profile'], hostedDomain:['manaiakalani.org']}
+	));
+	
+	app.get('/auth/google/callback', 
+		passport.authenticate('google', { failureRedirect: client + '/Home'}),
+		function(req, res) {
+			res.redirect(client + '/Lobby'); //potentially replace with google auth page??
+	});
 
 module.exports = app;
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Vue Test App"/>
+    <img alt="Vue logo" src="../assets/logo.png"><br><br>
+	<div class="googleButton" @click="googleLog">
+		<img src="../assets/gNormal.png" class="img-normal">
+		<img src="../assets/gPress.png" class="img-pressed">
+		<img src="../assets/gFocus.png" class="img-focused">
+	</div>
   </div>
 </template>
 <script>
-import HelloWorld from '../components/HelloWorld.vue'
-
+var config = require('../config.json');
+var server = config.serverAddress;
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  methods:{
+	  googleLog(event){
+		  event.preventDefault();
+		  window.open(server + '/auth/google');
+	  }
   }
 }
 
@@ -41,4 +48,9 @@ body {
 	margin-top:0px;
 	margin-bottom:0px;
 }
+.googleButton .img-normal{display:inline; z-index: 10}
+.googleButton .img-focused{display:none; z-index: 20}
+.googleButton .img-pressed{display:none; z-index: 30}
+.googleButton:hover .img-focused{display:inline} .googleButton:hover .img-normal{display:none; z-index: 10}
+.googleButton:active .img-pressed{display:inline} .googleButton:active .img-normal{display:none; z-index: 10} .googleButton:active .img-focused{display:none; z-index: 20}
 </style>
